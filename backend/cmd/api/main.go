39e6ba298eaa258e732b98b662d4725629b9d0f8
zperry4 <zactperry73@gmail.com>
2025-05-01 14:32:12 -0400

    comment out env stuff for now

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -3,20 +3,21 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
+	//"log"
 	"os"
 
-	"github.com/joho/godotenv"
+	//"github.com/joho/godotenv"
 
 	"CodeSynapse/internal"
 )
 
 func main() {
 	ctx := context.Background()
+  /* Comment out once we have actual keys
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
-	}
+	}*/
 
 	if err := internal.Run(ctx, os.Stdout); err != nil {
 		fmt.Fprintf(os.Stderr, "error (ctrlc): %v\n", err)
