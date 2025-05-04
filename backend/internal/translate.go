9cc46dafd2998c1b660542da4f283d0989c1dc27
zperry4 <zactperry73@gmail.com>
2025-05-03 23:46:50 -0400

    open ai api setup

--- backend/internal/translate.go
+++ backend/internal/translate.go
@@ -5,19 +5,24 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"os"
 	"strings"
+
+	"github.com/openai/openai-go"
+	"github.com/openai/openai-go/option"
 )
 
 /*
  * Map for storing best models for each translation
  */
 var bestTranslationMap = map[string]string{
-	"python-java": "CodeBERT",
-	"go-python":   "CodeBERT",
+	"python-java": "Transcoder",
+	"go-python":   "ChatGPT",
+	"c++-python":  "Transcoder",
 }
 
 /*
- * TranslateCode() 
+ * TranslateCode()
  */
 func TranslateCode(ctx context.Context, req TranslationRequest) (*TranslationResponse, error) {
 	model, ok := bestTranslationMap[req.Translation]
@@ -26,11 +31,20 @@ func TranslateCode(ctx context.Context, req TranslationRequest) (*TranslationRes
 	}
 
 	switch model {
-	case "CodeBERT":
+	case "ChatGPT":
 		log.Printf("    Translation supported -- using %s for %s\n", model, req.Translation)
-		//TranslateWithCodeBERT(ctx, req)
-	case "Codex":
-		log.Printf("    Translation supported -- using %s for %s\n", model, req.Translation)
+		prompt, err := BuildPrompt(ctx, req.Translation, req.Code)
+		if err != nil {
+			return nil, err
+		}
+
+		resp, err := OpenAIRequest(ctx, prompt)
+		if err != nil {
+			return nil, err
+		}
+
+		return resp, nil
+
 	case "Transcoder":
 		log.Printf("    Translation supported -- using %s for %s\n", model, req.Translation)
 	default:
@@ -54,6 +68,7 @@ func BuildPrompt(ctx context.Context, translation string, code string) (string, 
 	from := translationPair[0]
 	to := translationPair[1]
 
+  // TODO: Tune this, specify the exact format we want the response in to simplify parsing it easily.
 	prompt := fmt.Sprintf(
 		"Please translate the following %s code to the same equivalent in %s\nCODE: %s\n",
 		from,
@@ -63,3 +78,40 @@ func BuildPrompt(ctx context.Context, translation string, code string) (string, 
 
 	return prompt, nil
 }
+
+// TODO: Parse the response and get just the code
+func ParseModelResponse() { } 
+
+// Function to send request to OpenAI API
+func OpenAIRequest(ctx context.Context, prompt string) (*TranslationResponse, error) {
+	key := os.Getenv("OPEN_AI_KEY")
+
+	client := openai.NewClient(
+		option.WithAPIKey(key),
+	)
+
+	log.Println("OpenAI Request: building and sending chat")
+
+	chatCompletion, err := client.Chat.Completions.New(
+		ctx,
+		openai.ChatCompletionNewParams{
+			Messages: []openai.ChatCompletionMessageParamUnion{
+				openai.UserMessage(prompt),
+			},
+			Model: openai.ChatModelGPT4o,
+		},
+	)
+	if err != nil {
+		log.Println("   Open AI (ERROR) - Issue with completion: ", err)
+		return nil, err
+	}
+
+	log.Println("     Open AI: ", chatCompletion.Choices[0].Message.Content)
+  // TODO: Parse response
+
+	return nil, nil
+}
+
+// Function to send request to GCP service
+
+// Function to send request to Hugging Face? / Whatever else
